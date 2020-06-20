7ae064b047c70371a07fa2d35301872d0ceb8df6
goedelsoup <goedelsoup@gmail.com>
2020-06-19 20:06:59 -0400

    cleanup auth propagation

--- src/test/scala/compstak/nomad/NomadSuite.scala
+++ src/test/scala/compstak/nomad/NomadSuite.scala
@@ -5,8 +5,10 @@ import cats.effect.testing.scalatest.AsyncIOSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.freespec.AsyncFreeSpec
 import org.http4s.client.asynchttpclient.AsyncHttpClient
+import org.http4s.implicits._
 
 class NomadSuite extends AsyncFreeSpec with AsyncIOSpec with Matchers {
+  val auth = Anonymous(uri"http://localhost:4646")
   val client = AsyncHttpClient
     .allocate[IO]()
     .map(_._1)
--- src/test/scala/compstak/nomad/endpoints/AgentSuite.scala
+++ src/test/scala/compstak/nomad/endpoints/AgentSuite.scala
@@ -3,35 +3,34 @@ package endpoints
 
 import cats.effect._
 import cats.implicits._
-import org.http4s.implicits._
 
 class AgentSuite extends NomadSuite {
 
   "Agent" - {
     "members" in {
       agent
-        .members[IO](Anonymous(uri"http://localhost:4646"))
+        .members[IO](auth)
         .run(client)
         .map(_ => assert(true))
     }
 
     "servers" in {
       agent
-        .servers[IO](Anonymous(uri"http://localhost:4646"))
+        .servers[IO](auth)
         .run(client)
         .map(_ => assert(true))
     }
 
     "self" in {
       agent
-        .self[IO](Anonymous(uri"http://localhost:4646"))
+        .self[IO](auth)
         .run(client)
         .map(_ => assert(true))
     }
 
     "health" in {
       agent
-        .health[IO](Anonymous(uri"http://localhost:4646"))
+        .health[IO](auth)
         .run(client)
         .map(_ => assert(true))
     }

ba2dd5e930e4763ad936693ca39dafa121c0d510
goedelsoup <goedelsoup@gmail.com>
2020-06-20 17:54:59 -0400

    create parameterized job as a fixture

---
+++ src/test/scala/compstak/nomad/JobSpecs.scala
@@ -0,0 +1,31 @@
+package compstak.nomad
+
+object JobSpecs {
+
+  val echo: String =
+    s"""
+job "echo" {
+  type        = "batch"
+  region      = "global"
+  datacenters = ["dc1"]
+
+  parameterized {
+    payload       = "forbidden"
+    meta_required = ["word"]
+    meta_optional = []
+  }
+
+  group "echo" {
+    
+    task "echo" {
+      driver = "raw_exec"
+
+      config {
+        command = "echo"
+        args = ["$${NOMAD_META_word}"]
+      }
+    }
+  }
+}
+    """
+}
--- src/test/scala/compstak/nomad/NomadSuite.scala
+++ src/test/scala/compstak/nomad/NomadSuite.scala
@@ -2,15 +2,31 @@ package compstak.nomad
 
 import cats.effect.IO
 import cats.effect.testing.scalatest.AsyncIOSpec
+import cats.implicits._
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.freespec.AsyncFreeSpec
 import org.http4s.client.asynchttpclient.AsyncHttpClient
 import org.http4s.implicits._
+import org.scalatest.BeforeAndAfterAll
 
-trait NomadSuite extends AsyncFreeSpec with AsyncIOSpec with Matchers {
+trait NomadSuite extends AsyncFreeSpec with BeforeAndAfterAll with AsyncIOSpec with Matchers {
+
   val auth = Anonymous(uri"http://127.0.0.1:4646")
   val client = AsyncHttpClient
     .allocate[IO]()
     .map(_._1)
     .unsafeRunSync()
+
+  override def beforeAll(): Unit = {
+    import endpoints.jobs._
+    import data.Jobs
+
+    parse[IO](auth, data.Jobs.ParseJob(JobSpecs.echo, true))
+      .flatMap { job =>
+        create[IO](auth, Jobs.CreateJob(job))
+      }
+      .run(client)
+      .void
+      .unsafeRunSync()
+  }
 }
